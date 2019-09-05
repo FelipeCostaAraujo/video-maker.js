d814c753631b5edd0baf1469626ab86bf1b91e6a
FelipeAraujo <felipe.s-04@hotmail.com>
2019-09-05 00:05:57 -0300

    first commit master

---
+++ index.js
@@ -0,0 +1,24 @@
+const readline = require('readline-sync');
+const robots = {text: require('./robots/text.js')};
+
+async function start() {
+    const content = {};
+
+    content.searchTerm = askAndReturnSearchTerm();
+    content.prefix = askAndReturnPrefix();
+    await robots.text(content);
+
+    function askAndReturnSearchTerm() {
+        return readline.question('Termo de busca para o Wikipedia: ');
+    }
+
+    function askAndReturnPrefix(){
+        const prefixes = ['Quem e','O que e','A historia de'];
+        const selectedPrefixIndex = readline.keyInSelect(prefixes,'Escolha uma opcao: ');
+        const selectedPrefixText = prefixes[selectedPrefixIndex];
+        return selectedPrefixText;
+    }
+
+    console.log(content);
+}
+start();
