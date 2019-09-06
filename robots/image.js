1a5eea7dea72fa59155be4082527a010820f9f66
FelipeAraujo <felipe.s-04@hotmail.com>
2019-09-06 20:57:55 -0300

    download images

--- robots/image.js
+++ robots/image.js
@@ -2,14 +2,16 @@
 const google = require('googleapis').google;
 const customSearch = google.customsearch('v1');
 const googleSearchCredentials = require('./credentials/google-search.json');
+const imageDownloader = require('image-downloader');
 const state = require('./state.js');
 
 async function robot() {
     const content = state.load();
-
     await fetchImagesOfAllSentences(content);
 
-    state.save(content);
+    await downloadAllImages(content);
+
+    //state.save(content);
 
     async function fetchImagesOfAllSentences(content) {
         for (const sentence of content.sentences) {
@@ -36,7 +38,33 @@ async function robot() {
         return imagesUrl;
     }
 
+    async function downloadAllImages(content) {
+        content.downloadedImages = [];
 
+        for (let index in content.sentences) {
+            const images = content.sentences[index].images;
+            for (let ImageIndex in images) {
+                const imageUrl = images[ImageIndex];
+                try {
+                    if (content.downloadedImages.includes(imageUrl)) {
+                        throw new Error("Imagem ja foi baixada");
+                    }
+                    await downloadAndSaveImage(imageUrl, `${index}-original.png`);
+                    content.downloadedImages.push(imageUrl);
+                    console.log(`>${index} ${ImageIndex} Baixou imagem : ${imageUrl}`);
+                    break;
+                } catch (error) {
+                    console.log(`>${index} ${ImageIndex} Erro na imagem : ${imageUrl} `);
+                }
+            }
+        }
+    }
+    async function downloadAndSaveImage(url, fileName) {
+        return imageDownloader.image({
+            url: url,
+            dest: `./content/${fileName}`
+        });
+    }
 }
 
 module.exports = robot;
