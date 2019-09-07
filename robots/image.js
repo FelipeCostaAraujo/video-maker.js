6e9f1685ce3c46b484055bbd6e74053e2639be62
FelipeAraujo <felipe.s-04@hotmail.com>
2019-09-06 23:26:43 -0300

    convert complete

--- robots/image.js
+++ robots/image.js
@@ -8,12 +8,12 @@ const state = require('./state.js');
 
 async function robot() {
     const content = state.load();
-    //await fetchImagesOfAllSentences(content);
-    //await downloadAllImages(content);
-    //await convertAllImages(content);
-    //await createAllSentenceImages(content);
+    await fetchImagesOfAllSentences(content);
+    await downloadAllImages(content);
+    await convertAllImages(content);
+    await createAllSentenceImages(content);
     await createYoutubeThumbnail();
-    //state.save(content);
+    state.save(content);
 
     async function fetchImagesOfAllSentences(content) {
         for (const sentence of content.sentences) {
